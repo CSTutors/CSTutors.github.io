351aeb4a7779a21eab7e1f28050ac7c9bde4023c
Sophie Engle <sjengle@cs.usfca.edu>
2013-06-27 18:26:04 -0700

    Updated templates, added first guide.

--- js/cstutors.js
+++ js/cstutors.js
@@ -3,17 +3,17 @@
  */
 
 // add bootstrap css and javascript to head
-$.get("html/bootstrap.html", function(html) {
+$.get("/html/bootstrap.html", function(html) {
   $('head').append(html);
 }, "html");
 
 // add navigation bar to before container
-$.get("html/navigation.html", function(html) {
+$.get("/html/navigation.html", function(html) {
   $("body").prepend(html);
   $("li#nav-" + activeid).addClass("active");
 }, "html");
 
 // add footer bar to end of container
-$.get("html/footer.html", function(html) {
+$.get("/html/footer.html", function(html) {
   $(".container").append(html);
 });
